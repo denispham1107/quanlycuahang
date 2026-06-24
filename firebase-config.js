de28a20efa527628578c06277fb5a0773e38e5b7
denispham <minhquan117@gmail.com>
2026-06-25 01:55:57 +0700

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,10 +1,11 @@
 window.firebaseAppConfig = {
-  apiKey: "PASTE_YOUR_FIREBASE_CONFIG_HERE",
-  authDomain: "PASTE_YOUR_FIREBASE_CONFIG_HERE",
-  projectId: "PASTE_YOUR_FIREBASE_CONFIG_HERE",
-  storageBucket: "PASTE_YOUR_FIREBASE_CONFIG_HERE",
-  messagingSenderId: "PASTE_YOUR_FIREBASE_CONFIG_HERE",
-  appId: "PASTE_YOUR_FIREBASE_CONFIG_HERE"
+  apiKey: "AIzaSyAkmEnSQcNp173Q_OWVW-tmVSWVSBttyc8",
+  authDomain: "quanlycuahang-d3ab7.firebaseapp.com",
+  projectId: "quanlycuahang-d3ab7",
+  storageBucket: "quanlycuahang-d3ab7.firebasestorage.app",
+  messagingSenderId: "766184475989",
+  appId: "1:766184475989:web:875e0df8b2e5870bc6f3dc",
+  measurementId: "G-GCKHCFY4HF"
 };
 
 window.appCloudOptions = {
